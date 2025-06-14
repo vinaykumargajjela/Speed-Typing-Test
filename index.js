a3f2007d005f8c9908d52aac4c69c80af3c5ecaa
Gajjela Vinay Kumar <codewithvinay.dev@gmail.com>
2025-06-14 18:05:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ let timerId = setInterval(function() {
 }, 1000);
 
 resetBtnE1.addEventListener("click", function() {
-    quoteInputE1="";
+    quoteInputE1.value="";
     counter = 0;
     clearInterval(timerId);
     timerId = setInterval(function() {
